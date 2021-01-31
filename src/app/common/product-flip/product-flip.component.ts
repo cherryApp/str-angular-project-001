899a42618978596566ba74a13a14d4774bafc9b4
Várdai Enikő <vardai.eniko@gmail.com>
2021-01-31 13:08:06 +0100

    filter

--- src/app/common/product-flip/product-flip.component.ts
+++ src/app/common/product-flip/product-flip.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product-service';
+import { ProductCardComponent } from 'src/app/common/product-card/product-card.component';
+
 
 @Component({
   selector: 'app-product-flip',
   templateUrl: './product-flip.component.html',
   styleUrls: ['./product-flip.component.scss']
 })
-export class ProductFlipComponent implements OnInit {
+export class ProductFlipComponent implements OnInit {  
+  @Input() products: Product[] = [];
+  @Input() product: Product;
+  @Input() productK: Product;
+  @Input() productA: Product;  
+    
+  productList: Product[] = this.productService.list;
+  
+  lapozoListK: Product[] = this.productList.filter( product => product.featured )
+    .sort( () => 0.5 - Math.random())
+    .slice(0, 5);
+    
 
-  constructor() { }
+  lapozoListA: Product[] = this.productList.filter( product => product.active )
+    .sort( () => 0.5 - Math.random())
+    .slice(0, 5);
+
+  constructor(
+    private productService: ProductService,    
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -4,14 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-  transform(value: any[], phrase: string, key: string = ''): any[] {
-    if(!Array.isArray(value) || !phrase || !key){
+  transform(value: any[], phrase: string): any {
+    if (!phrase) {
       return value;
     }
-    phrase = (''+phrase).toLowerCase();
-    return value.filter((item)=>((''+item[key]).toLowerCase().includes(phrase)));
-  
-  }
-
+    phrase = ('' + phrase).toLowerCase();
+    return value.filter((item) => {
+      let isOK: boolean = false;
+      for (let k in item) {
+        let check = item[k].toString().toLowerCase();
+        if (check.indexOf(phrase) > -1) {
+          isOK = true;
+        }
+      }
+      return isOK;
+    });
 }
-
+}
