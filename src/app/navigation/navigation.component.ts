50b66f443640f654150a80510f2427f079bc8302
Adam <adam.nemeth.0315@gmail.com>
2021-02-01 17:05:49 +0100

    inputot visszaállítottam az eredeti megoldásra

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -12,17 +12,9 @@ export class NavigationComponent implements OnInit {
 
   appName: string = this.config.appName;
   menuItems: IMenuItem[] = this.config.menuItems;
-  productList: Product[] = this.productService.list;
-  phrase = '';
-
-  onChangePhrase(event: Event): void {
-    this.phrase = (event.target as HTMLInputElement).value;
-  }
-
 
   constructor(
     private config: ConfigService,
-    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
