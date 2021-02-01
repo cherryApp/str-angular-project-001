a5f95eeb766365ed7c83d784111adbdb237231c5
Adam <adam.nemeth.0315@gmail.com>
2021-02-01 12:25:10 +0100

    akciós könyvek flex beállítva

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 import { ConfigService, IMenuItem } from '../service/config.service';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-navigation',
@@ -10,9 +12,17 @@ export class NavigationComponent implements OnInit {
 
   appName: string = this.config.appName;
   menuItems: IMenuItem[] = this.config.menuItems;
+  productList: Product[] = this.productService.list;
+  phrase = '';
+
+  onChangePhrase(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
+  }
+
 
   constructor(
     private config: ConfigService,
+    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
