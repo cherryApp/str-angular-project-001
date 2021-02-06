d372f9b2332d4bbb07fdf6b3b62de1d074a7407c
Várdai Enikő <vardai.eniko@gmail.com>
2021-02-06 10:07:27 +0100

    featured-filter

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -10,8 +10,8 @@ import { ProductService } from '../../service/product-service';
 })
 export class ProductListComponent implements OnInit {
   @Input() products: Product[] = [];
-  @Input() kategoria1:Product[];
-  @Input() kategoria2: Product[];
+  @Input() kategoria1List:Product[];
+  @Input() kategoria2List: Product[];
   
   productList$: Observable<Product[]> = this.productService.getAll(); 
   
